2f6330befc5a03c0e4d8dc94048275a018c340ea
klich1984 <2239@holbertonschool.com>
2021-04-04 18:51:18 -0500

    06 | update file | testing comma

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install puppet-lint using puppet
 package { 'task-1':
-  name    => 'puppet-lint',
-  ensure  => '2.1.1',
-  provider => 'gem'
+  name     => 'puppet-lint',
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
