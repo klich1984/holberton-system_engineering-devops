cc44ce280c35bf767fda5d98546ae9243331dbbc
klich1984 <2239@holbertonschool.com>
2021-05-03 16:04:12 -0500

    00 | add script | script running and good output

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Task 0 """
+import requests
+from sys import argv
+import json
+
+
+if __name__ == '__main__':
+    def Requests_API(argv):
+        """script that returns information about his/her todo list progress.
+        for a given employee ID
+
+        Args:
+            argv ([str]): id that employed
+        """
+    req_all = requests.get("https://jsonplaceholder.typicode.com/todos/?userId="+str(argv[1]))
+    req_user = requests.get("https://jsonplaceholder.typicode.com/users/"+str(argv[1]))
+
+    json_t = req_all.json()
+    json_u = req_user.json()
+    user_name = json_u.get("name")
+
+    totaol_num_task = 0
+    num_done_task = 0
+    count = 0
+    list1 = []
+    for tasks in json_t:
+        totaol_num_task += 1
+        if tasks['completed'] == True:
+            list1 += [tasks['title']]
+            num_done_task +=2
+    print("Employee {} is done with tasks({}/{}):".format(user_name, num_done_task, totaol_num_task))
+
+    for count in list1:
+        print('\t{}'.format(count))
