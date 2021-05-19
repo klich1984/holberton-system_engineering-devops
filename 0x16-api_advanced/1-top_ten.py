7dc76998124d0e77b007ef3c38a90dee8ebd3cc1
klich1984 <2239@holbertonschool.com>
2021-05-18 21:09:33 -0500

    01 | update | erase print and change limit for 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,11 +11,11 @@ def top_ten(subreddit):
     titles of the first 10 hot posts listed for a given subreddit"""
     res = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
               headers={"User-Agent": "Klich from Holberton"},
-              allow_redirects=False, params={"limit": 3})
-    print(res)
+              allow_redirects=False, params={"limit": 10})
+    # print(res)
     if res.status_code == 200:
         for children in res.json().get('data').get('children'):
-            print(children)
+            # print(children)
             print(children.get('data').get('title'))
     else:
         print(None)
