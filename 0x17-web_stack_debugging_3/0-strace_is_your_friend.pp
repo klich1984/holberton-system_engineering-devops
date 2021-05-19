46c5da49ff03a1afe1124bc18588d9814f3e6c1f
klich1984 <2239@holbertonschool.com>
2021-05-19 16:50:14 -0500

    01 | update file | test2 path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Find out why Apache is returning a 500 error with strace
 exec { 'debugging':
-    provider => '/usr/bin/bash',
+    provider => '/usr/bin/sh',
     command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php"
 }
