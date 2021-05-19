8d961fb2d4be653e68ece798c0b941ea3605dfec
klich1984 <2239@holbertonschool.com>
2021-05-18 20:33:48 -0500

    00 | add file | code running and good output

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0.
+"""
+from requests import get
+from requests.api import head
+
+
+def number_of_subscribers(subreddit):
+    """ function that queries the Reddit API and returns the number of"""
+
+    res = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+              headers={"User-Agent": "Klich from Holberton"},
+              allow_redirects=False)
+    print(res)
+    if res.status_code == 200:
+        number_of_subscribers = res.json().get('data').get('subscribers')
+        return number_of_subscribers
+    else:
+        return 0
