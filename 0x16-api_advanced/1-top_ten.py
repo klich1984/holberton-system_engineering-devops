ae1053d89a2a2bbdaadb65d416c3de9ac05c1e80
klich1984 <2239@holbertonschool.com>
2021-05-18 21:08:02 -0500

    00 | add file | file running

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API and prints the
+    titles of the first 10 hot posts listed for a given subreddit"""
+    res = get("https://www.reddit.com/r/{}/hot.json".format(subreddit),
+              headers={"User-Agent": "Klich from Holberton"},
+              allow_redirects=False, params={"limit": 3})
+    print(res)
+    if res.status_code == 200:
+        for children in res.json().get('data').get('children'):
+            print(children)
+            print(children.get('data').get('title'))
+    else:
+        print(None)
